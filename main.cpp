8c8eea45c98e361f7669e85d15eeb717920d38c7
Bashmeg <superbashmeg@gmail.com>
2024-10-19 13:14:26 +0300

    Position

--- main.cpp
+++ main.cpp
@@ -1,145 +1,168 @@
 #include "lib.h"
 
-class Fraction
+
+namespace position
 {
-	int _numerator;
-	int _denominator;
-public:
-	Fraction() {
-		_numerator = 0;
-		_denominator = 1;
-	}
-	Fraction(int full, int numerator, int denominator) : Fraction()
+	class Vector1
 	{
-		_numerator = numerator + full*denominator;
-		_denominator = denominator;
-	}
-	Fraction(int numerator, int denominator) : Fraction()
-	{
-		_numerator = numerator;
-		_denominator = denominator;
-	}
-
-	void setNumerator(int numerator) { _numerator = numerator; }
-	void setDenominator(int denominator) { _denominator = denominator; }
-
-	int getNumerator() { return _numerator; }
-	int getDenominator() { return _denominator; }
-
-	void reduce() {
-		int gcd = 1;
-		int smaller = _numerator < _denominator ? _numerator : _denominator;
-		for (int i = 1; i <= smaller; i++) {
-			if (_numerator % i == 0 && _denominator % i == 0) {
-				gcd = i;
-			}
+	private:
+		int _x;
+	public:
+		Vector1() 
+		{
+			_x = 0;
 		}
-		_numerator /= gcd;
-		_denominator /= gcd;
-	}
-	void print()
-	{
-		if (_numerator != 0) {
-			cout << _numerator << "/" << _denominator;
+		Vector1(int x) 
+		{
+			_x = x;
 		}
-		cout << endl;
-	}
-	void printDecimal()
+		int GetVector()
+		{
+			return _x;
+		}
+		void SetVector(int x)
+		{
+			_x = x;
+		}
+		void MoveVector(int x)
+		{
+			_x += x;
+		}
+		void print()
+		{
+			cout << "Vector 1: " << _x << endl;
+		}
+	};
+	class Vector2
 	{
-		cout << double(_numerator) / double(_denominator) << endl;
-	}
-	void printWrongFraction()
+	private: 
+		int _x;
+		int _y;
+	public:
+		Vector2()
+		{
+			_x = 0;
+			_y = 0;
+		}
+		Vector2(int x, int y)
+		{
+			_x = x;
+			_y = y;
+		}
+		int GetVectorX()
+		{
+			return _x;
+		}
+		int GetVectorY()
+		{
+			return _y;
+		}
+		void SetVectorX(int x)
+		{
+			_x = x;
+		}
+		void SetVectorY(int y)
+		{
+			_y = y;
+		}
+		void MoveVectorX(int x)
+		{
+			_x += x;
+		}
+		void MoveVectorY(int y)
+		{
+			_y += y;
+		}
+		void print()
+		{
+			cout << "Vector 2: " << _x << " " << _y << endl;
+		}
+	};
+	class Vector3
 	{
-		int full = _numerator / _denominator;
-		int realNumerator = _numerator % _denominator;
-		cout << full << " " << realNumerator << "/" << _denominator << endl;
-	}
-};
+	private:
+		int _x;
+		int _y;
+		int _z;
+	public:
+		Vector3()
+		{
+			_x = 0;
+			_y = 0;
+			_z = 0;
+		}
+		Vector3(int x, int y, int z)
+		{
+			_x = x;
+			_y = y;
+			_z = z;
+		}
+		int GetVectorX()
+		{
+			return _x;
+		}
+		int GetVectorY()
+		{
+			return _y;
+		}
+		int GetVectorZ()
+		{
+			return _z;
+		}
+		void SetVectorX(int x)
+		{
+			_x = x;
+		}
+		void SetVectorY(int y)
+		{
+			_y = y;
+		}
+		void SetVectorZ(int z)
+		{
+			_z = z;
+		}
+		void MoveVectorX(int x)
+		{
+			_x += x;
+		}
+		void MoveVectorY(int y)
+		{
+			_y += y;
+		}
+		void MoveVectorZ(int z)
+		{
+			_z += z;
+		}
+		void print()
+		{
+			cout << "Vector 3: " << _x << " " << _y << " " << _z << endl;
+		}
+	};
 
-int GetSmallestCommonDenominator(int a, int b)
-{
-	int result = a;
-	while (result % b != 0)
-	{
-		result += a;
-	}
-	return result;
-}
-
-Fraction Add(Fraction f1, Fraction f2)
-{
-	Fraction result;
-	int smallestCommonDenominator = GetSmallestCommonDenominator(f1.getDenominator(), f2.getDenominator());
-	int f1Numerator = f1.getNumerator() * (smallestCommonDenominator / f1.getDenominator());
-	int f2Numerator = f2.getNumerator() * (smallestCommonDenominator / f2.getDenominator());
-	result.setNumerator(f1Numerator + f2Numerator);
-	result.setDenominator(smallestCommonDenominator);
-	result.reduce();
-	return result;
-}
-
-Fraction Subtract(Fraction f1, Fraction f2)
-{
-	Fraction result;
-	int smallestCommonDenominator = GetSmallestCommonDenominator(f1.getDenominator(), f2.getDenominator());
-	int f1Numerator = f1.getNumerator() * (smallestCommonDenominator / f1.getDenominator());
-	int f2Numerator = f2.getNumerator() * (smallestCommonDenominator / f2.getDenominator());
-	result.setNumerator(f1Numerator - f2Numerator);
-	result.setDenominator(smallestCommonDenominator);
-	result.reduce();
-	return result;
-}
-
-Fraction Multiply(Fraction f1, Fraction f2)
-{
-	Fraction result;
-	result.setNumerator(f1.getNumerator() * f2.getNumerator());
-	result.setDenominator(f1.getDenominator() * f2.getDenominator());
-	result.reduce();
-	return result;
-}
-
-Fraction Divide(Fraction f1, Fraction f2)
-{
-	Fraction result;
-	Fraction f2Inverse(f2.getDenominator(), f2.getNumerator());
-	result.setNumerator(f1.getNumerator() * f2Inverse.getNumerator());
-	result.setDenominator(f1.getDenominator() * f2Inverse.getDenominator());
-	result.reduce();
-	return result;
 }
 
 int main()
 {
-	Fraction f1(1, 2, 5);
-	f1.print();
-	f1.printDecimal();
-	f1.printWrongFraction();
+	position::Vector1 v1 = position::Vector1(5);
+	v1.print();
+	v1.MoveVector(10);
+	v1.print();
 	cout << endl;
-	Fraction f2(1, 3, 5);
-	f2.print();
-	f2.printDecimal();
-	f2.printWrongFraction();
+	position::Vector2 v2 = position::Vector2(5, 10);
+	v2.print();
+	v2.MoveVectorX(10);
+	v2.print();
+	v2.MoveVectorY(20);
+	v2.print();
 	cout << endl;
-	Fraction f3 = Add(f1, f2);
-	f3.print();
-	f3.printDecimal();
-	f3.printWrongFraction();
-	cout << endl;
-	Fraction f4 = Subtract(f1, f2);
-	f4.print();
-	f4.printDecimal();
-	f4.printWrongFraction();
-	cout << endl;
-	Fraction f5 = Multiply(f1, f2);
-	f5.print();
-	f5.printDecimal();
-	f5.printWrongFraction();
-	cout << endl;
-	Fraction f6 = Divide(f1, f2);
-	f6.print();
-	f6.printDecimal();
-	f6.printWrongFraction();
-	cout << endl;
+	position::Vector3 v3 = position::Vector3(5, 10, 15);
+	v3.print();
+	v3.MoveVectorX(10);
+	v3.print();
+	v3.MoveVectorY(20);
+	v3.print();
+	v3.MoveVectorZ(30);
+	v3.print();
+
 	return 0;
 }
